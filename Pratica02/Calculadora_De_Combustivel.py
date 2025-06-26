52248d3d66ee4b8e0c3f5d3e190c8e86238886df
Bruno Correa <bruno2005ferro@gmail.com>
2025-06-26 17:52:28 -0300

    Adicionando comentarios

--- Pratica02/Calculadora_De_Combustivel.py
+++ Pratica02/Calculadora_De_Combustivel.py
@@ -1,8 +1,11 @@
+#Informações sobre a distancia percorrida e combustivel gasto
 distancia_percorrida = 300 
 combustivel_gasto = 25
 
+#Calculo de consumo medio
 consumo_medio = distancia_percorrida / combustivel_gasto
 
+#Exibição para o usuário 
 print(f"A distância percorrida do veiculo foi {distancia_percorrida}Km")
 print(f"O combustivel gasto foi {combustivel_gasto}L")
 print(f"O consumo medio do carro é de {consumo_medio: .2f}")
--- Pratica02/Calculadora_De_Desconto.py
+++ Pratica02/Calculadora_De_Desconto.py
@@ -1,11 +1,15 @@
+#Informações sobre o produto
 nome_do_produto = "Camiseta"
 preco_original = 50.00
 porcentagem_de_desconto = 20
 
+#Cálculo do desconto
 valor_desconto = preco_original * (porcentagem_de_desconto / 100)
 
+#Cálculo do produto com o desconto
 preco_com_desconto = preco_original - valor_desconto
 
+#Exibição para usuário
 print(f"Nome do produto: {nome_do_produto}")
 print(f"Preço Original: R$ {preco_original: .2f}")
 print(f"Porcentagem do desconto: {porcentagem_de_desconto} %")
